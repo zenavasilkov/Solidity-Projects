b267c9e7808bbe14db8af24f04fa8dab728a5e70
zenavasilkov <zenavasilkov6@gmail.com>
2026-01-19 14:48:25 +0300

    Create FundMe project

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockM3Aggregator.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    uint8 public constant DECIMAL = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+    uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == SEPOLIA_CHAIN_ID) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMAL, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+
+        return anvilConfig;
+    }
+}

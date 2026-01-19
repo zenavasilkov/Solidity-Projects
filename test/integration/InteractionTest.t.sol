ac3bb50d4e89ab4996335e7faf8fbc56d4f5d139
zenavasilkov <zenavasilkov6@gmail.com>
2026-01-19 14:24:40 +0300

    Create FundMe project

---
+++ test/integration/InteractionTest.t.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "../../lib/forge-std/src/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTestIntegration is Test {}
---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "../../lib/forge-std/src/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() public {
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
+    }
+
+    function testMinDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), address(msg.sender));
+    }
+
+    function testFundFailsWithoutEnoghETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+}
